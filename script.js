eba7bc9ec24e3599327c341e3e213b7e4aef6c69
skhemnan <saahil.khemlani@gmail.com>
2020-06-01 00:54:31 -0700

    Fixed search icon and finished expand section

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function handleExpand(){
 	$('.hidden').hide();
+	$('.show-less').hide();
+	$('.expanded-btn').hide();
 	$('.expand-btn').on('click', function(event){
 		$('.hidden').toggle('fast');
+		$('.show-less').toggle();
+		$('.expand').toggle();
+		$('.expanded-btn').toggle();
+		$('.expand-btn').toggle();
+	})
+	$('.expanded-btn').on('click', function(event){
+		$('.hidden').toggle('fast');
+		$('.show-less').toggle();
+		$('.expand').toggle();
+		$('.expanded-btn').toggle();
+		$('.expand-btn').toggle();
 	})
 }
 
