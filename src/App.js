98a661e58f3aae893da94a67e5011099c80d7721
skhemnan <saahil.khemlani@gmail.com>
2020-12-24 19:10:50 +0700

    Added intro dialog and fixed margin errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './App.css';
 
+/* Component Imports */
+import Info from './components/Info'
+
 function App() {return (
   <div class="background">
+		<Info />
   </div>
   );}
 
---
+++ src/components/Info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Info = () => {
+	return (
+		<div class="info">
+			<h1 class="title">Hello, I'm Saahil</h1>
+		</div>
+	);
+};
+
+export default Info;
