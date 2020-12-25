35ce337b8e13f15a7818fdec92ba0ceb82de58b7
skhemnan <saahil.khemlani@gmail.com>
2020-12-25 11:56:59 +0700

    Added open modal buttons

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 /* Component Imports */
 import Info from './components/Info'
+import ModalButton from './components/Button'
 
 function App() {return (
   <div class="background">
+		<ModalButton arrow='down' title='About myself & my experience'/>
 		<Info />
+		<ModalButton arrow='up' title='Projects and resume'/>
   </div>
   );}
 
