d4da5470ddc1160a84498907c0827654463e4ecf
skhemnan <saahil.khemlani@gmail.com>
2020-12-31 13:18:12 +0700

    CSS Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css';
+import './index.css'
 
 /* Component Imports */
 import Info from './components/Info'
-import ModalButton from './components/Button'
+import ModalButton from './components/ModalButton'
 
-function App() {return (
+function App() {
+
+	return (	
   <div class="background">
-		<ModalButton arrow='down' title='About myself & my experience'/>
+		<ModalButton position='up' title='About myself & my experience'/>
 		<Info />
-		<ModalButton arrow='up' title='Projects and resume'/>
+		<ModalButton position='down' title='Projects and resume'/>
   </div>
   );}
 
