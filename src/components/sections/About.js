897ab93b3be0738d98517001b6aa517b166b8206
Saahil Khemnani <saahil.khemlani@gmail.com>
2021-09-07 17:59:24 +0700

    removed pictures and moved logo to the top

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -13,27 +13,27 @@ import ironman from '../../assets/ironman.gif'
 const About = () => {
 return (
 	<>
+		<div class="about-icon">
+			<img src={hello} style={{width: 'auto', height: 100}}/> 
+		</div>
 		<AnimatePresence>
 				<div class="about-container">
 					<div class="about-section">
 						<p class="about-text">
-							<div style={{alignItems: 'center'}}>
-								<img src={hello} style={{width: '70%', height: '50%', marginLeft: '15%'}}/> 
-							</div>
 						{/* <p class="icon"><FiUserPlus/></p> */}
 							Hello friend! I'm a full-stack web and app developer based in Jakarta, Indonesia, formerly Southern California, USA. 
 							I have 2 years of experience with <a class="skill">React.js</a> and <a class="skill">React Native</a> (with <a class="skill">Redux</a>, <a class="skill">Redux-thunk</a> and <a class="skill">React Context/Hooks</a>) To add some function to my shiny front end I also have experience in <a class="skill">Node.js,</a> <a class="skill">Express.js</a>, and <a class="skill">PostgreSQL</a> 
 							{' '}to complete the stack. I'm all about serverless, having worked with <a class="skill">Firebase</a> and <a class="skill">Google Cloud</a>. To avoid any mishaps and late nights troubleshooting, I'm also proficient in <a class="skill">Jest</a> and <a class="skill">Appium</a>. 
-							I'm a huge fan of lightweight and modular components with functional design in mind. I also have a pension for writing automations and workflows with <a class="skill">Bash</a> and <a class="skill">Git</a> that make my job easier. 
+							I'm a huge fan of lightweight and modular components with functional design in mind. I also enjoy writing automations and workflows with <a class="skill">Bash</a> and <a class="skill">Git</a> that make my job easier. I love working in a collaborative environment and believe in complete transparency and visbility in the workplace. 
 						</p>
 					</div>
 					<div class='about-section'>
 						<p class='about-text' id='about-work'>
 							I'm currently a full stack software engineer at <a class="skill" href={'https://techcrunch.com/2021/02/03/gajigesa-a-fintech-startup-serving-underbanked-indonesian-workers-raises-2-5-million-seed-round/'}>GajiGesa</a>, a startup dedicated to bringing financial and employee wellness to 60% of Indonesia's workforce.
-							<div style={{width: '100%', height: '70%', padding: 10, alignItems: 'center', justifyContent: 'center'}}>
+							{/* <div style={{width: '100%', height: '70%', padding: 10, alignItems: 'center', justifyContent: 'center'}}>
 								<img src={'https://techcrunch.com/wp-content/uploads/2021/02/GajiGesa_Team_Photos_New.jpeg?w=1390&crop=1'} style={{maxWidth: '95%', height: 'auto',}}/> 
-							</div>
-							At GajiGesa, my tasks include but are not limited to building employer back end infrastructure for clients, 
+							</div> */}
+							{' '}At GajiGesa, my tasks include but are not limited to building employer back end infrastructure for clients, 
 							and contributing to the development, maintenance and improvement of our React Native apps, <a class="skill" href={'https://play.google.com/store/apps/details?id=com.gajitimnative'}>GajiTim</a> and <a class="skill" href={'https://play.google.com/store/apps/details?id=com.gajigesanative'}>GajiGesa</a>. 
 							Prior to that, I worked as a data analyst at <a class="skill" href={'http://www.bitvore.com'}>Bitvore</a>, 
 							{/* <a class="icon" id="check"><FiCheckCircle/></a>  */}
@@ -50,7 +50,7 @@ return (
 							{' '}to find and rank the top 10 movies that I've seen in my life, and <a class="skill" href={'http://skhemnan.github.io/visitr'}>Visitr</a> (<a class="skill" href={"https://github.com/skhemnan/visitr"}>Repo</a>) to quickly find out what I should pack in my suitcase based on my destination's weather. I'm always looking 
 							for a challenge and collaborative team that puts learning and growth first. When I'm not coding, you can find me watching Marvel movies, eating a Chipotle burrito, or staying up late at night watching tech conferences from around the world. I'd love to chat! Browse my socials by clicking the links below or send me an email anytime!
 							<div style={{width: '100%', height: 'auto', padding: 10, alignItems: 'center', justifyContent: 'center'}}>
-								<img src={ironman} style={{maxWidth: '95%', height: 'auto', maxHeight: 'center'}}/> 
+								{/* <img src={ironman} style={{maxWidth: '95%', height: 'auto', maxHeight: 'center'}}/>  */}
 							</div>
 						</p>
 					</div>
